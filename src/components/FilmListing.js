ee63757af8cdc0809ea5e2e9c626f053b5f169f0
Nick Nedev <nedevn@gmail.com>
2018-01-22 20:05:50 -0800

    Initial Commit

---
+++ src/components/FilmListing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FilmListings = (props) => {
+  return (
+    <div className="film-list">
+      <h1 className="section-title">FILMS</h1>
+    </div>
+  )
+}
+export default FilmListings
+
+
